be821180bb0d9c2c7488b13e4e2c0ff76b425a50
Naufal Arif <na713634@gmail.com>
2026-05-14 18:00:22 +0700

    fouth case fos exiting the menu and the program

--- stack.cpp
+++ stack.cpp
@@ -114,4 +114,7 @@ int main()
                 cout << "Stack is empty.No top value." << endl;
             }
             break;
+        case 4:
+            cout << "Exiting program." << endl;
+            break;
         
