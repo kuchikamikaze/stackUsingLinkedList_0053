7805a164fe63a74533df2622bff265f8c0c216d4
Naufal Arif <na713634@gmail.com>
2026-05-14 17:53:35 +0700

    if the stack is not empty then display it

--- stack.cpp
+++ stack.cpp
@@ -53,4 +53,14 @@ public:
         {
             cout << "List is empty." << endl;
         }
-        
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
